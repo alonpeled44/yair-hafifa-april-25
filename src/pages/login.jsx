0c3afb67288a98380042a98cf98a73eded599946
yair <yairbenyosef0@gmail.com>
2025-04-22 11:04:26 +0300

    convert to jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,36 +1,62 @@
-import styles from "@/styles/login.module.css";
+import React, { useState } from "react";
+import { isUserValid, welcomeGuest } from "@/lib/auth";
 import loginImage from "@/assets/images/log-in-image.png";
+import styles from "@/pages/login.module.css";
 
 export default function Login() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleNameChange({ target }) {
+    const regex = /^[A-Za-z0-9]*$/;
+    if (regex.test(target.value)) setName(target.value);
+  }
+
+  function handlePasswordChange({ target }) {
+    setPassword(target.value);
+  }
+
+  function checkInputs() {
+    isUserValid({ name, password });
+  }
+
+  function joinAsGuess() {
+    welcomeGuest();
+  }
+
   return (
     <main className={styles.main}>
-      <form>
+      <form onSubmit={(e) => e.preventDefault()}>
         <img src={loginImage.src} alt="log-in" />
         <h1>Log in to your pokemon account</h1>
         <section className={styles["user-data"]}>
           <label for={"username-input"}>
             Username
-            <input type={"text"} id={"username-input"} required />
+            <input
+              value={name}
+              onChange={handleNameChange}
+              type={"text"}
+              id={"username-input"}
+              required
+            />
           </label>
 
           <label for={"password-input"}>
             Password
-            <input type={"password"} id={"password-input"} required />
+            <input
+              value={password}
+              onChange={handlePasswordChange}
+              type={"password"}
+              id={"password-input"}
+              required
+            />
           </label>
         </section>
         <div className={styles.buttons}>
-          <button
-            type={"submit"}
-            name={"input-text"}
-            onclick={"isUserValid('username-input', 'password-input')"}
-          >
+          <button type={"submit"} name={"input-text"} onClick={checkInputs}>
             Log in
           </button>
-          <button
-            type={"submit"}
-            name={"input-text"}
-            onclick={"welcomeGuest()"}
-          >
+          <button type={"submit"} name={"input-text"} onClick={joinAsGuess}>
             Join as guest
           </button>
         </div>
