73b308b46f76f15100f913b67af8b6f0f6e015eb
alonpeled44 <163134682+alonpeled44@users.noreply.github.com>
2025-04-23 10:11:15 +0300

    remove the start of the convert for checking it in the PR

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,3 @@
-import styles from "@/styles/components/header.module.css";
-import pokemonLogo from "@/assets/images/pokemon-logo.png";
-
 export default function Header() {
-  const date = new Date().toLocaleDateString("he-IL");
-
-  return (
-    <header className={styles.header}>
-      <div className={styles["pokemon-info"]}>
-        <img src={pokemonLogo.src} alt={"pokemon-logo"} />
-        <p>Pok`emon</p>
-      </div>
-      <p>{date}</p>
-    </header>
-  );
+  return <></>;
 }
