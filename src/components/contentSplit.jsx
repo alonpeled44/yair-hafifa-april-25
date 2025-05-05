d106b7368a484fdd4aff456246efd40eea8007f9
yair <yairbenyosef0@gmail.com>
2025-05-05 14:49:49 +0300

    edit the content split looks

--- src/components/contentSplit.jsx
+++ src/components/contentSplit.jsx
@@ -1,7 +1,13 @@
 export default function ContentSplit() {
   return (
     <div
-      style={{ width: "3px", alignSelf: "stretch", background: "black" }}
+      style={{
+        width: "3px",
+        background: "black",
+        alignSelf: "stretch",
+        marginTop: "5px",
+        marginBottom: "5px",
+      }}
     ></div>
   );
 }
