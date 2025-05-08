ae49f4520e95fe0c7dfd745e447e532810b4b217
yair <yairbenyosef0@gmail.com>
2025-05-08 10:29:50 +0300

    added pokedex

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,19 @@
 import styles from "@/styles/components/pokemonCard.module.css";
 
-export default function PokemonCard({ name, img, type, weight, height }) {
+export default function PokemonCard({
+  name,
+  frontViewImageUrl,
+  type,
+  weight,
+  height,
+}) {
   return (
     <div className={styles["pokemon-card"]}>
-      <p>{name}</p>
-      <img>{img}</img>
-      <p>{type}</p>
-      <p>{weight}</p>
-      <p>{height}</p>
+      <p>name: {name}</p>
+      <img src={frontViewImageUrl}></img>
+      <p>type: {type}</p>
+      <p>weight: {weight}</p>
+      <p>height: {height}</p>
     </div>
   );
 }
