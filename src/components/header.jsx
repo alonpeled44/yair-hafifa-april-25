0007e6fd055f01a89d16475a2a71fbd42e9b1c3e
yair <yairbenyosef0@gmail.com>
2025-06-03 16:00:57 +0300

    broooo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,13 +2,17 @@ import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/router";
 import VerticalDivider from "./VerticalDivider";
+import Modal from "./Modal";
 import Link from "next/link";
 import pokemonLogo from "@/assets/images/pokemon-logo.png";
+import settingsIcon from "@/assets/images/settings.png";
 import styles from "@/styles/components/header.module.css";
 
 export default function Header({ newUsersDefaultPage }) {
   const [username, setUsername] = useState("");
   const pathname = usePathname();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const router = useRouter();
 
@@ -17,33 +21,87 @@ export default function Header({ newUsersDefaultPage }) {
   }, [pathname]);
 
   return (
-    <header className={styles.header}>
-      <div className={styles["pokemon-info"]}>
-        <img src={pokemonLogo.src} alt={"pokemon-logo"} />
-        <p>Pok`emon</p>
-        {username && (
-          <>
-            <VerticalDivider />
-            <div className={styles["header-items"]}>
-              <p>{username}</p>
-              <button
-                onClick={() => {
-                  localStorage.removeItem("username");
-                  router.push(newUsersDefaultPage);
-                }}
-              >
-                Log out
-              </button>
+    <>
+      <header className={styles.header}>
+        <div className={styles.right}>
+          <img src={pokemonLogo.src} alt={"pokemon-logo"} />
+          <p>Pok`emon</p>
+          {username && (
+            <>
+              <VerticalDivider />
+              <div className={styles["right-items"]}>
+                <p>{username}</p>
+                <button
+                  onClick={() => {
+                    localStorage.removeItem("username");
+                    router.push(newUsersDefaultPage);
+                  }}
+                >
+                  Log out
+                </button>
+              </div>
+
+              <VerticalDivider />
+
+              <Link href="/">PokeDex</Link>
+            </>
+          )}
+        </div>
+
+        <div className={styles.left}>
+          <p>{new Date().toLocaleDateString("he-IL")}</p>
+          <button onClick={() => setIsModalOpen(true)}>
+            <img src={settingsIcon.src} alt="settings Icon" />
+          </button>
+        </div>
+      </header>
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          handleClose={() => {
+            setIsModalOpen(false);
+          }}
+        >
+          <div className={styles["modal-content-wrapper"]}>
+            <div className={styles.theme}>
+              <p>Theme</p>
+
+              <div className={styles.options}>
+                <label>
+                  <input type="radio" name="theme" value="light" />
+                  <span>🌞</span>
+                </label>
+
+                <label>
+                  <input type="radio" name="theme" value="dark" />
+                  <span>🌙</span>
+                </label>
+              </div>
             </div>
 
-            <VerticalDivider />
+            <div className={styles["font-size"]}>
+              <p>Font</p>
 
-            <Link href="/">PokeDex</Link>
-          </>
-        )}
-      </div>
+              <div className={styles.options}>
+                <label>
+                  <input type="radio" name="theme" value="small" />
+                  <span>🗛</span>
+                </label>
 
-      <p>{new Date().toLocaleDateString("he-IL")}</p>
-    </header>
+                <label>
+                  <input type="radio" name="theme" value="medium" />
+                  <span>🗛</span>
+                </label>
+
+                <label>
+                  <input type="radio" name="theme" value="large" />
+                  <span>🗛</span>
+                </label>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      )}
+    </>
   );
 }
