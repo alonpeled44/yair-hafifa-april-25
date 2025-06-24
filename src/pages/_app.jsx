8aaeb34e9d1c9ea495fc50339554b3c349820186
yair <102980343+ya1r-cohen@users.noreply.github.com>
2025-06-24 15:29:01 +0300

    darkmode

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import WindowWidthProvider from "@/contexts/WindowWidthProvider";
 import { users } from "@/lib/users";
@@ -6,8 +6,16 @@ import Header from "@/components/header";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  const [theme, setTheme] = useState("");
+  const [font, setFont] = useState("");
+
   const router = useRouter();
   const newUsersDefaultPage = "/login";
+
+  useEffect(() => {
+    setTheme(localStorage.getItem("theme"));
+    setFont(localStorage.getItem("font"))
+  }, [])
 
   useEffect(() => {
     const savedUsername = localStorage.getItem("username");
@@ -19,11 +27,36 @@ export default function App({ Component, pageProps }) {
     else router.push(newUsersDefaultPage);
   }, []);
 
+ useEffect(() => {
+  document.body.classList.remove("light", "dark");
+
+  if (theme === "dark") {
+    document.body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.body.classList.add("light");
+    localStorage.setItem("theme", "light");
+  }
+
+  document.body.classList.remove("small", "medium", "large");
+
+  if (font === "small") {
+    document.body.classList.add("small");
+    localStorage.setItem("font", "small");
+  } else if (font === "large") {
+    document.body.classList.add("large");
+    localStorage.setItem("font", "large");
+  } else {
+    document.body.classList.add("medium");
+    localStorage.setItem("font", "medium");
+  }
+}, [theme, font]);
+
   return (
     <WindowWidthProvider>
-      <Header newUsersDefaultPage={newUsersDefaultPage} />
+      <Header newUsersDefaultPage={newUsersDefaultPage} theme={theme} setTheme={setTheme} font={font} setFont={setFont}/>
       <main>
-        <Component {...pageProps} />
+        <Component {...pageProps} theme={theme}/>
       </main>
     </WindowWidthProvider>
   );
